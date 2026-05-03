62b40b0fcea5e9b58cbc444fb705dd9587a5d020
Matt <mletnyre@gmail.com>
2026-05-02 22:10:12 -0500

    fixed the game seg faulting when moving between rooms

--- src/Room.h
+++ src/Room.h
@@ -4,12 +4,11 @@
 //only one of these on the screen at a time, go through doors to move between them. 
 #include <cstdlib>
 #include "Com.h"
+#include <cstring>
 
 class Room{
     public:
     Room(int y, int x, int wx, int wy): x_dim(x), y_dim(y), world_x(wx), world_y(wy), roomMap(y, std::vector<char>(x)){}
-
-    Room(){}
 
     std::vector<Com> getComs(){return this->coms;}
 
@@ -35,9 +34,9 @@ class Room{
         int i, j;
         int x_dim = this->getXDim();
         int y_dim = this->getYDim();
-        for(i = 0; i < x_dim-1; i++){
-            for(j = 0; j < y_dim-1; j++){
-                if(i == 0 || j == 0 || i == x_dim - 2 || j == y_dim - 2){ //the edges
+        for(i = 0; i < x_dim; i++){
+            for(j = 0; j < y_dim; j++){
+                if(i == 0 || j == 0 || i == x_dim-1 || j == y_dim -1 ){ //the edges
                     this->setRoomMap(j,i,'#');
                 }
                 else{
@@ -104,17 +103,17 @@ class Room{
     void placeDoors(){
         //pick a random spot for each door on all 4 sides
         
-        int nDoor = (rand() % x_dim) + 1;
-        int sDoor = (rand() % x_dim) + 1;
+        int nDoor = (rand() % x_dim -1) + 1;
+        int sDoor = (rand() % x_dim -1) + 1;
 
-        int wDoor = (rand() % y_dim ) + 1;
-        int eDoor = (rand() % y_dim ) + 1;
+        int wDoor = (rand() % y_dim -1) + 1;
+        int eDoor = (rand() % y_dim -1) + 1;
 
         setRoomMap(0, nDoor, '*');//set n Door
-        setRoomMap(y_dim - 2, sDoor, '*');//set s Door
+        setRoomMap(y_dim - 1, sDoor, '*');//set s Door
         
         setRoomMap(wDoor, 0,  '*');//set w Door
-        setRoomMap(eDoor, x_dim - 2, '*');//set e Door
+        setRoomMap(eDoor, x_dim - 1, '*');//set e Door
 
         setDoors(nDoor, sDoor, eDoor, wDoor);
     }
